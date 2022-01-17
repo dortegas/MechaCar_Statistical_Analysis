694e91b2bc4b3e4013791e4f2876841481d2e5fd
dortegas <90233528+dortegas@users.noreply.github.com>
2022-01-16 21:43:59 -0600

    Challenge deliverable

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,18 @@ suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsF
 total_summary <- suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 # 4. Group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Deliverable 3: T-Tests on Suspension Coils
+# 1. Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+# randomly sample 30 data points
+suspension_sample = suspension_table %>% sample_n(30)
+# One-Sample t-Test
+# Comparing sample versus suspensions mean
+t.test(suspension_sample$PSI,mu=1500)
+
+# 2. Determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+# One-Sample t-Test
+# Comparing subsets versus suspensions mean
+t.test(subset(suspension_table, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(suspension_table, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(suspension_table, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
